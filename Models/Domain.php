101cf88742ced03736c679e39cb25d94b4da19c6
Vasil Sokolov <vasil@afterlogic.com>
2023-01-23 23:21:57 +0200

    Updated dates in copyright

--- Models/Domain.php
+++ Models/Domain.php
@@ -10,7 +10,7 @@ namespace Aurora\Modules\MailDomains\Models;
 /**
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  * 
  * @property int $TenantId Identifier of tenant which contains the domain.
  * @property int $MailServerId Identifier of mail server which contains the domain.
